e581269d4eecd90cc5e2fcb5c8b2ec95eac7aa0c
Benoit <benoit.dubus@outlook.fr>
2021-04-21 15:06:17 +0200

    03 reducer

---
+++ PHP/exercices/chapitre2/03_reducer.php
@@ -0,0 +1,20 @@
+<?php
+// Créez maintenant un reducer à l'aide d'une fonction de callback passée en paramètre de votre fonction.
+
+$f = function (int $accumulateur, int $valeurCourante) {
+    $accumulateur += $valeurCourante;
+    return $accumulateur;
+};
+
+function my_reduce(array $numbers, callable $fn, int $initial = 0): int
+{
+    $sum = $initial;
+    foreach ($numbers as $number) {
+        $sum = $fn($sum, $number);
+    }
+    return $sum;
+}
+
+$numbers = [1, 2, 3, 4];
+
+echo my_reduce($numbers, $f, 0);
