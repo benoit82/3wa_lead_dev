0d62f45ce67b5ea92d05858648ba06e7c00f0046
Benoit <benoit.dubus@outlook.fr>
2021-04-22 11:34:34 +0200

    update

--- PHP/exercices/chapitre3/03_read.php
+++ PHP/exercices/chapitre3/03_read.php
@@ -14,6 +14,6 @@ function readChar($file, $modulo = 2)
     fclose($f);
 }
 
-$gen = readChar('./content/char.txt', 2);
+$gen = readChar('./char.txt', 2);
 
 foreach ($gen as $line) echo $line . PHP_EOL;
