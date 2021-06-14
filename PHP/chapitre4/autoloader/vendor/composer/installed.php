e2f2574e19c79feebae773e902f474bd7a83a83a
Benoit <benoit.dubus@outlook.fr>
2021-06-14 09:18:48 +0200

    add auto loader

---
+++ PHP/chapitre4/autoloader/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'da9426666f72e66fd661f1e39902d7f3eb8e2cce',
+    'name' => 'bntdb/autoloader',
+  ),
+  'versions' => 
+  array (
+    'bntdb/autoloader' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'da9426666f72e66fd661f1e39902d7f3eb8e2cce',
+    ),
+  ),
+);
