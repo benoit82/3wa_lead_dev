5722a054b30ede04710e8483b584adb89cfd8100
Benoit Dubus <benoit.dubus@abtasty.com>
2021-09-09 16:00:28 +0200

    update

--- week05_SOLID/02-SOLID/form/app.php
+++ week05_SOLID/02-SOLID/form/app.php
@@ -8,22 +8,22 @@ use Form\TextField;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$submit = new SubmitInput("submit", "Valider", "Valider", "btnSubmit");
+$submit = new SubmitInput("submit", "Valider", "Valider");
 
 $form = new ComponentForm(new SplObjectStorage, $submit);
 
-$firstName = new TextField("fName", "Firstname", "", "fNameField");
+$firstName = new TextField("fName", "Firstname", "");
 $form->addToStorage($firstName);
 
-$lastName = new TextField("lName", "Lastname", "", "lNameField");
+$lastName = new TextField("lName", "Lastname", "");
 $form->addToStorage($lastName);
 
-$age = new NumberField("age", "Age", "", "ageField");
+$age = new NumberField("age", "Age", "");
 $age->setMin(18);
 $age->setMax(130);
 $form->addToStorage($age);
 
-$password = new PasswordField("password", "Password", "", "passwordField", [], isRequired: true);
+$password = new PasswordField("password", "Password", "", [], isRequired: true);
 $form->addToStorage($password);
 
 $form->displayForm();
