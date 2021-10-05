f4648000c856414ab269f02f4abea9b6566fac59
Benoit Dubus <benoit.dubus@abtasty.com>
2021-10-05 11:41:49 +0200

    1.3.1 + 1.3.2

--- week06_TDD/chapitre1_introduction/Exercices/Exercice_03_Model/tests/UserTest.php
+++ week06_TDD/chapitre1_introduction/Exercices/Exercice_03_Model/tests/UserTest.php
@@ -8,54 +8,84 @@ use App\Model;
 class UserTest extends TestCase
 {
 
-    protected $model;
+  protected $model;
+  protected $users;
 
-    public function setUp(): void
-    {
-        $this->pdo = new \PDO('sqlite::memory:');
-        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+  public function setUp(): void
+  {
+    $this->pdo = new \PDO('sqlite::memory:');
+    $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
-        $this->pdo->exec(
-            "CREATE TABLE IF NOT EXISTS user
+    $this->pdo->exec(
+      "CREATE TABLE IF NOT EXISTS user
           (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username VARCHAR( 225 )
           )
             "
-        );
+    );
 
-    }
+    $this->model = new Model($this->pdo);
+    $this->users = [
+      ['username' => 'Alan'],
+      ['username' => 'Sophie'],
+      ['username' => 'Bernard'],
+    ];
+  }
 
-    /**
-     * @test count method insert
-     */
-    public function testSeedsCreate()
-    {
+  /**
+   * @test count method insert
+   */
+  public function testSeedsCreate()
+  {
+    $this->model->hydrate($this->users);
+    $usersInDb = $this->model->all();
 
-    }
+    $this->assertEquals(count($this->users), count($usersInDb));
+  }
 
-    /**
-     * @test save method insert
-     */
-    public function testInsertSave()
-    {
+  /**
+   * @test save method insert
+   */
+  public function testInsertSave()
+  {
+    $benoit = new User;
+    $benoit->username = "Benoit";
+    $this->model->save($benoit);
 
-    }
+    $this->assertEquals($this->model->find(1), $this->model->all()[0]);
+  }
 
-     /**
-     * @test save method update
-     */
-    public function testUpdateSave()
-    {
-     
-    }
+  /**
+   * @test save method update
+   */
+  public function testUpdateSave()
+  {
+    $this->model->hydrate($this->users);
+    $userAlan = $this->model->find(1);
+    $this->assertEquals($userAlan->username, "Alan");
 
-    /**
-     * @test delete resource by id
-     */
-    public function testDelete()
-    {
+    $userAlan->username = "Benoit";
+    $this->model->update($userAlan);
+    $userId1 = $this->model->find(1);
+    
+    $this->assertEquals($userAlan->username, $userId1->username);
+    $this->assertNotEquals($userId1->username, "Alan");
+    $this->assertEquals($userId1->username, "Benoit");
+  }
 
-    }
+  /**
+   * @test delete resource by id
+   */
+  public function testDelete()
+  {
+    $this->model->hydrate($this->users);
+    $userAlan = $this->model->find(1);
+    $this->model->delete(1);
 
+    $nbUsersDb = count($this->model->all());
+
+    $this->assertEquals($nbUsersDb, count($this->users)-1);
+    $this->assertNotContains($userAlan, $this->model->all());
+  }
 }
