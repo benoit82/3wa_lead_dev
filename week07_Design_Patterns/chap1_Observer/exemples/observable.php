94d715e2dfd3ff6a2363f515a26be1a1d1e89d97
Benoit Dubus <benoit.dubus@abtasty.com>
2021-11-02 10:28:50 +0100

    ajout exemple observer

---
+++ week07_Design_Patterns/chap1_Observer/exemples/observable.php
@@ -0,0 +1,54 @@
+<?php
+
+class User implements SplSubject
+{
+
+    private $id;
+
+    public function attach(SplObserver $observer)
+    {
+        $this->observers[] = $observer;
+    }
+
+    public function detach(SplObserver $observer)
+    {
+    }
+
+    public function notify()
+    {
+
+        foreach ($this->observers as $value) {
+            $value->update($this);
+        }
+    }
+
+    public function create(string $name, string $email): void
+    {
+
+        $this->id = uniqid(true); // quelque chose s'est passé dans la classe User que l'on souhaite notifier
+        $this->notify();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
+class Log implements SplObserver
+{
+
+    public function update(SplSubject $subject)
+    {
+        echo "log :" . $subject->getId() . "\n";
+    }
+}
+
+$user = new User;
+
+$user->attach(new Log);
+$user->attach(new Log);
+$user->attach(new Log);
+$user->attach(new Log);
+
+$user->create('Alan', 'alan@cochonou.fr');
